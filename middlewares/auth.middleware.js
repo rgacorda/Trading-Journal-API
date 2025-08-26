fff708b19ebed1362e0e9e8b4eabc0af31356005
Ronjay Gervacio Acorda <rgacorda@Ronjays-MacBook-Pro.local>
2025-08-26 09:20:04 +0800

    fix: token

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -8,7 +8,7 @@ const verifyToken = (req, res, next) => {
   }
 
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const decoded = jwt.verify(token, process.env.JWT_REFRESH_SECRET);
     req.user = decoded;
     next();
   } catch (err) {
